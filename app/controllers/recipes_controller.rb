d599c07cc048da0df3da048b078e03df5b921d17
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-28 10:55:39 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
     redirect_to recipes_path
   end
 
-  private 
+  private
 
   def recipe_params
     params.require(recipe).permit(:title, ingredients_attributes[:id, :quantity, :name])
